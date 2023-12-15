1c4ae32c0d00a241d8467a76390caf417bd79905
hairongchen <hairong.chen@intel.com>
2023-12-15 16:34:01 +0800

    c72

--- vmsdk/rust/sample/src/cc-sample.rs
+++ vmsdk/rust/sample/src/cc-sample.rs
@@ -1,11 +1,12 @@
 use cctrusted::{get_cc_report, ExtraArgs};
+use anyhow::anyhow
 
 fn main() {
     let nonce = "MTIzNDU2Nzg=".to_string();
     let data = "YWJjZGVmZw==".to_string();
 
     match get_cc_report(nonce, data, ExtraArgs{}){
-        Ok(q) => println!("quote: {}", q),
+        Ok(q) => println!("quote: {}", q.len()),
         Err(e) => {
             return Err(anyhow!(
                 "[get_cc_report] error getting TDX report: {:?}",
