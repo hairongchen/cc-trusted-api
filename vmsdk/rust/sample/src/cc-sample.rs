5140499b5479a3754ea9810a00e6bc5f89f5ca5d
hairongchen <hairong.chen@intel.com>
2023-12-15 17:04:57 +0800

    c77

--- vmsdk/rust/sample/src/cc-sample.rs
+++ vmsdk/rust/sample/src/cc-sample.rs
@@ -7,7 +7,7 @@ fn main() {
 
     // retrieve cc report
     match get_cc_report(nonce, data, ExtraArgs{}){
-        Ok(q) => println!("quote: {}", q.len()),
+        Ok(q) => println!("quote length: {}", q.len()),
         Err(e) => {
             println!("[get_cc_report] error getting TDX report: {:?}",e);
         }
