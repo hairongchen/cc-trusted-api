0a3677e2000ed5e7c6a69e53b61894be77d584af
hairongchen <hairong.chen@intel.com>
2023-12-19 15:46:40 +0800

    c200

--- common/rust/cctrusted_base/src/tdx/tdvm.rs
+++ common/rust/cctrusted_base/src/tdx/tdvm.rs
@@ -82,7 +82,7 @@ impl CVM for TdxVM {
         match self.get_tdx_quote(report_data) {
             Ok(q) => Ok(q),
             Err(e) => return Err(anyhow!("[get_cc_report] error getting TDX quote: {:?}", e)),
-        };
+        }
 
     }
 
