aabe7823b227c2773cec53d1a33cf2a070af4f99
hairongchen <hairong.chen@intel.com>
2023-12-21 10:06:46 +0800

    c274

--- vmsdk/rust/cctrusted/src/api.rs
+++ vmsdk/rust/cctrusted/src/api.rs
@@ -9,20 +9,20 @@ use cctrusted_base::tcg::{TcgDigest, ALGO_NAME_MAP};
 
 use crate::api_data::*;
 
-/*** 
+/***
     Get the cc report for given nonce and data.
 
     The cc report is signing of attestation data (IMR values or hashes of IMR
     values), made by a trusted foundation (TPM) using a key trusted by the
     verifier.
-    
+
     Different trusted foundation may use different cc report format.
 
     Args:
         nonce (String): against replay attacks
         data (String): user data
         extraArgs: for TPM, it will be given list of IMR/PCRs
-    
+
     Returns:
         The cc report byte array or error information
 */
@@ -46,19 +46,19 @@ pub fn dump_cc_report(report: Vec<u8>) -> Result<(), anyhow::Error> {
 }
 
 /***
-    Get measurement register according to given selected index and algorithms
+   Get measurement register according to given selected index and algorithms
 
-    Each trusted foundation in CC environment provides the multiple measurement
-    registers, the count is update to ``get_measurement_count()``. And for each
-    measurement register, it may provides multiple digest for different algorithms.
+   Each trusted foundation in CC environment provides the multiple measurement
+   registers, the count is update to ``get_measurement_count()``. And for each
+   measurement register, it may provides multiple digest for different algorithms.
 
-    Args:
-        index (u8): the index of measurement register,
-        algo_id (u8): the alrogithms ID
-    
-    Returns:
-        TcgDigest struct
- */
+   Args:
+       index (u8): the index of measurement register,
+       algo_id (u8): the alrogithms ID
+
+   Returns:
+       TcgDigest struct
+*/
 pub fn get_cc_measurement(_index: u8, _algo_id: u8) -> TcgDigest {
     todo!()
 }
@@ -68,7 +68,7 @@ pub fn get_cc_measurement(_index: u8, _algo_id: u8) -> TcgDigest {
 
     TCG log in Eventlog. Verify to spoof events in the TCG log, hence defeating
     remotely-attested measured-boot.
-    
+
     To measure the full CC runtime environment, the eventlog may include addtional
     OS type and cloud native type event beyond the measured-boot.
 
