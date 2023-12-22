490f0051522a6b8b0e18c9c4119211e46630a745
hairongchen <hairong.chen@intel.com>
2023-12-22 13:12:31 +0800

    c351

--- vmsdk/rust/cctrusted/src/api.rs
+++ vmsdk/rust/cctrusted/src/api.rs
@@ -140,7 +140,7 @@ impl ParseCcReport<CcTdxReport> for CcReport{
                 unsafe {
                     let report_ref: &CcTdxReport = mem::transmute(&tdx_quote);
                     //let report_copy = report_ref;
-                    Ok(*report_copy)
+                    Ok(*report_ref)
                 }
             },
             Err(e) => {
