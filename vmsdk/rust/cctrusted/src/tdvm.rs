31b34ab836469a9c1e3d1b30d5cf90440ec6bfad
hairongchen <hairong.chen@intel.com>
2023-12-26 15:35:52 +0800

    c48

--- vmsdk/rust/cctrusted/src/tdvm.rs
+++ vmsdk/rust/cctrusted/src/tdvm.rs
@@ -110,13 +110,13 @@ impl TdxVM {
                     let td_report: [u8; TDX_REPORT_LEN as usize] = [0; TDX_REPORT_LEN as usize];
                 
                     //build the request
-                    Ok(tdx_1_0_report_req {
+                    tdx_1_0_report_req {
                         subtype: 0 as u8,
                         reportdata: ptr::addr_of!(report_data_array) as u64,
                         rpd_len: REPORT_DATA_LEN,
                         tdreport: ptr::addr_of!(td_report) as u64,
                         tdr_len: TDX_REPORT_LEN,
-                    })
+                    };
 
                     //build the operator code
                     ioctl_readwrite!(
