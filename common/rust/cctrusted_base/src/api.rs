5ccfeac2f76052a2599fb749e452883d60416efd
hairongchen <hairong.chen@intel.com>
2023-12-26 16:28:30 +0800

    c65

--- common/rust/cctrusted_base/src/api.rs
+++ common/rust/cctrusted_base/src/api.rs
@@ -11,48 +11,48 @@ use core::result::Result::Ok;
 
 pub trait CCTrustedApi {
     /***
-    Get the cc report for given nonce and data.
+        Get the cc report for given nonce and data.
 
-    The cc report is signing of attestation data (IMR values or hashes of IMR
-    values), made by a trusted foundation (TPM) using a key trusted by the
-    verifier.
+        The cc report is signing of attestation data (IMR values or hashes of IMR
+        values), made by a trusted foundation (TPM) using a key trusted by the
+        verifier.
 
-    Different trusted foundation may use different cc report format.
+        Different trusted foundation may use different cc report format.
 
-    Args:
-        nonce (String): against replay attacks
-        data (String): user data
-        extraArgs: for TPM, it will be given list of IMR/PCRs
+        Args:
+            nonce (String): against replay attacks
+            data (String): user data
+            extraArgs: for TPM, it will be given list of IMR/PCRs
 
-    Returns:
-        The cc report byte array or error information
+        Returns:
+            The cc report byte array or error information
     */
     fn get_cc_report(nonce: String, data: String, _extra_args: ExtraArgs) -> Result<CcReport, anyhow::Error>;
 
     /***
-    Dump the given cc report in hex and char format
+        Dump the given cc report in hex and char format
 
-    Args:
-        report (Vec<u8>): cc report to be printed
+        Args:
+            report (Vec<u8>): cc report to be printed
 
-    Returns:
-        None
+        Returns:
+            None
     */
     fn dump_cc_report(report: &Vec<u8>);
 
     /***
-     Get measurement register according to given selected index and algorithms
+        Get measurement register according to given selected index and algorithms
 
-    Each trusted foundation in CC environment provides the multiple measurement
-    registers, the count is update to ``get_measurement_count()``. And for each
-    measurement register, it may provides multiple digest for different algorithms.
+        Each trusted foundation in CC environment provides the multiple measurement
+        registers, the count is update to ``get_measurement_count()``. And for each
+        measurement register, it may provides multiple digest for different algorithms.
 
-    Args:
-        index (u8): the index of measurement register,
-        algo_id (u8): the alrogithms ID
+        Args:
+            index (u8): the index of measurement register,
+            algo_id (u8): the alrogithms ID
 
-    Returns:
-        TcgDigest struct
+        Returns:
+            TcgDigest struct
     */
     fn get_cc_measurement(_index: u8, _algo_id: u8) -> TcgDigest;
 
@@ -104,7 +104,7 @@ pub trait ParseCcReport<T> {
     fn parse_cc_report(_report: Vec<u8>) -> Result<T, anyhow::Error>;
 }
 
-// this function parses cc report to the TdxQuote struct
+// this function parses raw cc report to the TdxQuote struct
 impl ParseCcReport<TdxQuote> for CcReport {
     fn parse_cc_report(report: Vec<u8>) -> Result<TdxQuote, anyhow::Error> {
         match TdxQuote::parse_tdx_quote(report) {
@@ -117,7 +117,7 @@ impl ParseCcReport<TdxQuote> for CcReport {
     }
 }
 
-// this function parses cc report to the TpmQuote struct
+// this function parses raw cc report to the TpmQuote struct
 impl ParseCcReport<TpmQuote> for CcReport {
     fn parse_cc_report(_report: Vec<u8>) -> Result<TpmQuote, anyhow::Error> {
         todo!()
