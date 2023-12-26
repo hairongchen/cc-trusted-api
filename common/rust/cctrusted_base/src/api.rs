7aaf86eb5a027f7cec4556cfb478652885af4397
hairongchen <hairong.chen@intel.com>
2023-12-26 14:15:31 +0800

    c14

--- common/rust/cctrusted_base/src/api.rs
+++ common/rust/cctrusted_base/src/api.rs
@@ -90,3 +90,23 @@ pub trait CCTrustedApi {
 pub trait ParseCcReport<T> {
     fn parse_cc_report(_report: Vec<u8>) -> Result<T, anyhow::Error>;
 }
+
+// this function parses cc report to the TDX quote struct
+impl ParseCcReport<TdxQuote> for CcReport {
+    fn parse_cc_report(report: Vec<u8>) -> Result<TdxQuote, anyhow::Error> {
+        match TdxQuote::parse_tdx_quote(report) {
+            Ok(tdx_quote) => unsafe {
+                let report: &TdxQuote = mem::transmute(&tdx_quote);
+                Ok(report.clone())
+            },
+            Err(e) => return Err(anyhow!("[parse_cc_report] error parse tdx quote: {:?}", e)),
+        }
+    }
+}
+
+// this function parses cc report to the TPM quote struct
+impl ParseCcReport<TpmQuote> for CcReport {
+    fn parse_cc_report(_report: Vec<u8>) -> Result<TpmQuote, anyhow::Error> {
+        todo!()
+    }
+}
