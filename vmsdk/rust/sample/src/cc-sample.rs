2eca63209c15c0ccc5fc223d623c11499ce42d5e
hairongchen <hairong.chen@intel.com>
2024-01-05 13:28:15 +0800

    c1

--- vmsdk/rust/sample/src/cc-sample.rs
+++ vmsdk/rust/sample/src/cc-sample.rs
@@ -22,35 +22,5 @@ fn main() {
         }
     };
 
-    // dump the cc report with API "dump_cc_report"
-    info!("call cc trusted API [dump_cc_report] to dump cc report!");
-    API::dump_cc_report(&report.cc_report);
 
-    // parse the cc report with API "parse_cc_report"
-    if report.cc_type == TeeType::TDX {
-        let tdx_quote: TdxQuote = match CcReport::parse_cc_report(report.cc_report) {
-            Ok(q) => q,
-            Err(e) => {
-                error!("error parse tdx quote: {:?}", e);
-                return;
-            }
-        };
-        info!(
-            "dummy_var1 = {}, dummy_var2 = {}",
-            tdx_quote.dummy_var1, tdx_quote.dummy_var2
-        );
-    }
-
-    // get CVM default algorithm with API "get_default_algorithm"
-    info!("call cc trusted API [get_default_algorithm] to get CVM supported algorithm!");
-    match API::get_default_algorithm() {
-        Ok(algorithm) => {
-            info!("supported algorithm: {}", algorithm.algo_id_str);
-            ();
-        }
-        Err(e) => {
-            error!("error get algorithm: {:?}", e);
-            return;
-        }
-    };
 }
