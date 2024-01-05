c126e471837cf4a870ea7209b75a44010a17d9df
hairongchen <hairong.chen@intel.com>
2024-01-05 09:19:09 +0800

    c53

--- common/rust/cctrusted_base/src/tdx/report.rs
+++ common/rust/cctrusted_base/src/tdx/report.rs
@@ -165,6 +165,7 @@ impl TdInfo {
         let rtmr2 = data[352..400].try_into().unwrap();
         let rtmr3 = data[400..448].try_into().unwrap();
 
+        info!("=========== data: {:?} ",data);
         info!("=========== rtmr0: {:?} ",rtmr0);
         info!("=========== rtmr1: {:?} ",rtmr1);
         info!("=========== rtmr2: {:?} ",rtmr2);
