2e655a7923e8d944b16ee779d75a4359a695a382
hairongchen <hairong.chen@intel.com>
2024-01-10 19:31:55 +0800

    c98

--- vmsdk/rust/cctrusted_vm/src/tdvm.rs
+++ vmsdk/rust/cctrusted_vm/src/tdvm.rs
@@ -341,7 +341,8 @@ impl CVM for TdxVM {
         let mut ccel_reader = BufReader::new(ccel_file);
         let mut ccel = Vec::new();
         ccel_reader.read_to_end(&mut ccel)?;
-        if (ccel.len() > 0).not() || (ccel[0..4].to_vec() != "CCEL") {
+        let ccel_str = vec!['a', 'b', 'c', 'd']
+        if (ccel.len() > 0).not() || (ccel[0..4].to_vec() != ccel_str) {
             return Err(anyhow!("[process_cc_eventlog] Invalid CCEL table"));
         }
 
