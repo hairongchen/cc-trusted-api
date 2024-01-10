a152d94cce77acaec39fbe33745a51dd2a79ce04
hairongchen <hairong.chen@intel.com>
2024-01-10 20:13:21 +0800

    c101

--- vmsdk/rust/sample/src/cc-sample.rs
+++ vmsdk/rust/sample/src/cc-sample.rs
@@ -21,7 +21,7 @@ fn main() {
     };
 
     // retrieve cc eventlog with API "get_cc_eventlog"    
-    let event_logs = match API::get_cc_eventlog() {
+    let event_logs = match API::get_cc_eventlog(None,None) {
         Ok(q) => q,
         Err(e) => {
             error!("error getting TDX report: {:?}", e);
