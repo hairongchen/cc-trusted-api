2eebec29853d5af841695ecd557aeeaf85184ed7
hairongchen <hairong.chen@intel.com>
2024-01-10 16:18:49 +0800

    c73

--- common/rust/cctrusted_base/src/eventlog.rs
+++ common/rust/cctrusted_base/src/eventlog.rs
@@ -99,7 +99,6 @@ impl TcgEventLog {
             let imr = get_u32(self.data[index..index+4].to_vec());
             index = index + 4;
             let event_type = get_u32(self.data[index..index+4].to_vec());
-            index = index + 4;
 
             if imr == 0xFFFFFFFF {
                 break;
