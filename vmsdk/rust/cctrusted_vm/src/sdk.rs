eb3145e28b5cca9021b94354d925c4df6dba706f
hairongchen <hairong.chen@intel.com>
2024-01-12 10:57:20 +0800

    c15

--- vmsdk/rust/cctrusted_vm/src/sdk.rs
+++ vmsdk/rust/cctrusted_vm/src/sdk.rs
@@ -85,3 +85,48 @@ impl CCTrustedApi for API {
         }
     }
 }
+
+#[cfg(test)]
+mod sdk_api_tests {
+    use super::*;
+    use rand::Rng;
+    use cctrusted_base::tdx::report::*;
+
+    #[test]
+    // test on cc trusted API [get_cc_report]
+    fn test_get_cc_report() {
+        let nonce = base64::encode(rand::thread_rng().gen::<[u8; 32]>());
+        let data = base64::encode(rand::thread_rng().gen::<[u8; 32]>());
+
+        let expected_report_data = match Tdx::generate_tdx_report_data(nonce, Some(data)) {
+            Ok(r) => r,
+            Err(e) => {
+                return Err(anyhow!(
+                    "[test_get_cc_report] error generating TDX report data: {:?}",
+                    e
+                ))
+            }
+        };
+
+        let report = match API::get_cc_report(nonce, data, ExtraArgs {}) {
+            Ok(q) => q,
+            Err(e) => {
+                error!("[test_get_cc_report] error getting TDX report: {:?}", e);
+                return;
+            }
+        };
+
+        if report.cc_type == TeeType::TDX {
+            let tdx_quote: TdxQuote = match CcReport::parse_cc_report(report.cc_report) {
+                Ok(q) => q,
+                Err(e) => {
+                    error!("[test_get_cc_report] error parse tdx quote: {:?}", e);
+                    return;
+                }
+            };
+
+            assert_eq!(base64::encode(&tdx_quote.body.report_data), expected_report_data);
+        }
+    }
+
+}
