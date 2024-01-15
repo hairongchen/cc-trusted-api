6bc52556f447b184694eb1a3b403bb4f51f05367
hairongchen <hairong.chen@intel.com>
2024-01-15 12:35:02 +0800

    168

--- vmsdk/rust/sample/src/cc-sample.rs
+++ vmsdk/rust/sample/src/cc-sample.rs
@@ -7,21 +7,9 @@ fn main() {
     // set log level
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
-    let nonce = "MTIzNDU2Nzg=".to_string();
-    let data = "YWJjZGVmZw==".to_string();
-
-    // retrieve cc report with API "get_cc_report"
-    info!("call cc trusted API [get_cc_report] to retrieve cc report!");
-    let _report = match API::get_cc_report(nonce, data, ExtraArgs {}) {
-        Ok(q) => q,
-        Err(e) => {
-            error!("error getting TDX report: {:?}", e);
-            return;
-        }
-    };
-
     // retrieve cc eventlog with API "get_cc_eventlog"    
-    let event_logs = match API::get_cc_eventlog(Some(1),Some(10)) {
+    //let event_logs = match API::get_cc_eventlog(Some(1),Some(10)) {
+    let event_logs = match API::get_cc_eventlog(None, None) {
         Ok(q) => q,
         Err(e) => {
             error!("error getting TDX report: {:?}", e);
