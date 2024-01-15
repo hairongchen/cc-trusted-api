09f1220c79ed76cfb4a9d53677c1c4bf7318e0c6
hairongchen <hairong.chen@intel.com>
2024-01-15 11:10:21 +0800

    154

--- common/rust/cctrusted_base/src/tcg.rs
+++ common/rust/cctrusted_base/src/tcg.rs
@@ -279,10 +279,14 @@ impl TcgImrEvent{
 
 impl TcgPcClientImrEvent{
     pub fn show(&self) {
+        let event_type_str = match TcgEventType::get_event_type_string(self.event_type) {
+            Ok(str) => str,
+            Err(e) => format!("{:?}", e),
+        };
+
         info!("--------------------Header Specification ID Event--------------------------");
         info!("IMR               : {}", self.imr_index);
-        info!("Type              : {:02X?} ({:?})", self.event_type,
-        TcgEventType::get_event_type_string(self.event_type));
+        info!("Type              : {:02X?} ({:?})", self.event_type, event_type_str);
         info!("Event:");
         dump_data(&self.event);
     }
