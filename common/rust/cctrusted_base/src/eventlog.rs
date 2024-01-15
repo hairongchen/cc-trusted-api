bad27a2f6366a6316e4d325beac87fe29d3f6605
hairongchen <hairong.chen@intel.com>
2024-01-15 10:29:28 +0800

    123

--- common/rust/cctrusted_base/src/eventlog.rs
+++ common/rust/cctrusted_base/src/eventlog.rs
@@ -51,7 +51,7 @@ impl TcgEventLog {
             }
         }
 
-        let begin = match start {
+        let begin: usize = match start {
             Some(s) => {
                 if s >= self.count {
                     return Err(anyhow!("[select] Invalid input start. Start must be number larger than 0 and smaller than total event log count."));
@@ -61,7 +61,7 @@ impl TcgEventLog {
             None => 0
         };
 
-        let end = match count {
+        let end:- usize = match count {
             Some(c) => {
                 if c <= 0 || c >= self.count {
                     return Err(anyhow!("[select] Invalid input count. count must be number larger than 0 and smaller than total event log count."));
