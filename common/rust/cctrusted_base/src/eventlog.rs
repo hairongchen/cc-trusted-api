b6b702c0e80bed4c7a42fe1ba00a5621e9ef2c32
hairongchen <hairong.chen@intel.com>
2024-01-24 14:43:19 +0800

    c51

--- common/rust/cctrusted_base/src/eventlog.rs
+++ common/rust/cctrusted_base/src/eventlog.rs
@@ -473,7 +473,7 @@ impl EventLogs {
             A struct containing the replay result arranged by IMR index and hash algorithm. 
             Layer 1 key of the struct is the IMR index, the value is another dict which using the
             hash algorithm as the key and the replayed measurement as value.
-            Sample value:
+            Sample results:
                 [
                     0: [{ 4: <measurement_replayed>},{ 12: <measurement_replayed>},]
                     1: { 12: <measurement_replayed>},
