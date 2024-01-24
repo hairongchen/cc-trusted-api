599bb8fecbfcdde25057c0f09b6e20259f94b169
hairongchen <hairong.chen@intel.com>
2024-01-24 15:45:22 +0800

    c63

--- common/rust/cctrusted_base/src/eventlog.rs
+++ common/rust/cctrusted_base/src/eventlog.rs
@@ -75,7 +75,7 @@ impl TcgEventLog {
         info!("imr_index = {}", self.imr_index);
         info!("event_type = {}", self.event_type);
         for index in 0..self.digests.len() {
-            info!("digest[{}] = {:?}", index, self.digests[index]);
+            info!("digest[{}] = {:?}", self.digests[index].algo_id, self.digests[index].hash);
         }
         info!("event_size = {}", self.event_size);
         info!("event = {:?}", self.event);
