c8d8f09377eab70f7f46f564ae79faebf50ae07a
hairongchen <hairong.chen@intel.com>
2024-01-24 15:42:57 +0800

    c61

--- common/rust/cctrusted_base/src/eventlog.rs
+++ common/rust/cctrusted_base/src/eventlog.rs
@@ -231,6 +231,7 @@ impl EventLogs {
                         self.event_logs
                             .push(event_log.format_event_log(self.parse_format));
                         self.count += 1;
+                        self.event_logs[self.count-1].show();
                     }
                     Err(e) => {
                         return Err(anyhow!(
