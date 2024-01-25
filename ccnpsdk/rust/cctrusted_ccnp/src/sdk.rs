ccc670f51aab686d8a92347da760d61c732bba37
hairongchen <hairong.chen@intel.com>
2024-01-25 15:14:43 +0800

    c2

---
+++ ccnpsdk/rust/cctrusted_ccnp/src/sdk.rs
@@ -0,0 +1,40 @@
+use anyhow::*;
+
+pub struct API {}
+
+impl CCTrustedApi for API {
+    // CCTrustedApi trait function: get cc report from CCNP server
+    fn get_cc_report(
+        nonce: Option<String>,
+        data: Option<String>,
+        _extra_args: ExtraArgs,
+    ) -> Result<CcReport, anyhow::Error> {
+        let channel = Endpoint::try_from("http://[::]:40081")
+            .unwrap()
+            .connect_with_connector(service_fn(|_: Uri| {
+                let path = "/run/ccnp/uds/quote-server.sock";
+                UnixStream::connect(path)
+            }))
+            .await
+            .unwrap();
+
+        let mut client = GetQuoteClient::new(channel);
+
+        let request = tonic::Request::new(GetQuoteRequest {
+            nonce,
+            user_data: data
+        });
+
+        let response = client.get_quote(request).await.unwrap().into_inner();
+        let cc_report = match base64::decode(response.quote) {
+            Ok(v) => v,
+            Err(e) => return Err(anyhow!("cc report is not base64 encoded: {:?}", e)),
+        };
+
+        Ok(CcReport{
+            cc_report: cc_report,
+            cc_type: TeeType::TDX
+        })
+
+    }
+}
