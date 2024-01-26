d5f78ea9c647b78f9baf921a50fdac3999329262
hairongchen <hairong.chen@intel.com>
2024-01-26 09:55:38 +0800

    c1

---
+++ ccnpsdk/rust/cctrusted_ccnp/src/client.rs
@@ -0,0 +1,67 @@
+use cctrusted_base::api_data::ExtraArgs;
+use core::result::Result::Ok;
+use tonic::transport::{Endpoint, Uri};
+use tonic::Request;
+use tower::service_fn;
+use crate::client::quote_server::get_quote_client::GetQuoteClient;
+use crate::client::quote_server::GetQuoteRequest;
+use crate::client::quote_server::GetQuoteResponse;
+use tokio::net::UnixStream;
+
+pub mod quote_server {
+    tonic::include_proto!("quoteserver");
+
+    pub(crate) const FILE_DESCRIPTOR_SET: &[u8] =
+        tonic::include_file_descriptor_set!("quote_server_descriptor");
+}
+
+pub struct CcnpClient{
+    pub uds_path: String,
+}
+
+impl CcnpClient {
+    async fn get_cc_report_from_server_async(
+        &self,
+        nonce: Option<String>,
+        data: Option<String>,
+        _extra_args: ExtraArgs,
+    ) -> Result<GetQuoteResponse, anyhow::Error> {
+        let channel = Endpoint::try_from("http://[::]:0")
+            .unwrap()
+            .connect_with_connector(service_fn(|_: Uri| {
+                //TODO: use parameters
+                UnixStream::connect("/run/ccnp/uds/quote-server.sock")
+            }))
+            .await
+            .unwrap();
+
+        let mut client = GetQuoteClient::new(channel);
+
+        let request = Request::new(GetQuoteRequest {
+            nonce: nonce.unwrap(),
+            user_data: data.unwrap()
+        });
+
+        let response = client.get_quote(request).await.unwrap().into_inner();
+        Ok(response)
+    }
+
+    // turn async call to sync call
+    pub fn get_cc_report_from_server(
+        &self,
+        nonce: Option<String>,
+        data: Option<String>,
+        extra_args: ExtraArgs,
+    ) -> Result<GetQuoteResponse, anyhow::Error> {
+        let response = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(self.get_cc_report_from_server_async(
+            nonce,
+            data,
+            extra_args
+        ));
+        response
+    }
+}
---
+++ ccnpsdk/rust/cctrusted_ccnp/src/sdk.rs
@@ -0,0 +1,78 @@
+use anyhow::*;
+use cctrusted_base::api::CCTrustedApi;
+use cctrusted_base::api_data::ExtraArgs;
+use cctrusted_base::api_data::CcReport;
+use cctrusted_base::api_data::Algorithm;
+use cctrusted_base::tcg::TcgDigest;
+use cctrusted_base::tcg::EventLogEntry;
+use core::result::Result::Ok;
+use cctrusted_base::cc_type::TeeType;
+use base64;
+use crate::client::CcnpClient;
+use cctrusted_base::binary_blob::dump_data;
+
+pub struct API {}
+
+impl CCTrustedApi for API {
+//impl API {
+    // CCTrustedApi trait function: get cc report from CCNP server
+    fn get_cc_report(
+        nonce: Option<String>,
+        data: Option<String>,
+        extra_args: ExtraArgs,
+    ) -> Result<CcReport, anyhow::Error> {
+
+        let ccnp_client = CcnpClient{
+            uds_path: "/run/ccnp/uds/quote-server.sock".to_string(),
+        };
+
+        let response = match ccnp_client.get_cc_report_from_server(nonce, data, extra_args){
+            Ok(r) => r,
+            Err(e) => {
+                return Err(anyhow!("[get_cc_report] err get cc report: {:?}", e));
+            }
+        };
+
+        //TODO: need to fix the quote server response
+        let report = match base64::decode(&response.quote.trim_matches('\"')) {
+                Ok(r) => r,
+            Err(e) => {
+                return Err(anyhow!("[get_cc_report] cc report is not base64 encoded: {:?}", e));
+            }
+        };
+
+        Ok(CcReport{
+            cc_report: report,
+            //TODO: need to fix 
+            cc_type: TeeType::TDX
+        })
+    }
+
+    // CCTrustedApi trait function: dump report of a CVM in hex and char format
+    fn dump_cc_report(report: &Vec<u8>) {
+        dump_data(report)
+    }
+
+    // CCTrustedApi trait function: get max number of CVM IMRs
+    fn get_measurement_count() -> Result<u8, anyhow::Error> {
+        todo!()
+    }
+
+    // CCTrustedApi trait function: get measurements of a CVM
+    fn get_cc_measurement(index: u8, algo_id: u16) -> Result<TcgDigest, anyhow::Error> {
+        todo!()
+    }
+
+    // CCTrustedApi trait function: get eventlogs of a CVM
+    fn get_cc_eventlog(
+        start: Option<u32>,
+        count: Option<u32>,
+    ) -> Result<Vec<EventLogEntry>, anyhow::Error> {
+        todo!()
+    }
+
+    // CCTrustedApi trait function: get default algorithm of a CVM
+    fn get_default_algorithm() -> Result<Algorithm, anyhow::Error> {
+        todo!()
+    }
+}
