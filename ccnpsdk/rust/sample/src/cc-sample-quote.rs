2983373bc55b47a745963e4e9ede32ae613cd5e3
hairongchen <hairong.chen@intel.com>
2024-01-26 10:01:43 +0800

    c5

--- ccnpsdk/rust/sample/src/cc-sample-quote.rs
+++ ccnpsdk/rust/sample/src/cc-sample-quote.rs
@@ -28,6 +28,8 @@ fn main() {
         }
     };
 
+    info!("length of the cc report is: {}", report.cc_report.len());
+
     // dump the cc report with API "dump_cc_report"
     //info!("call cc trusted API [dump_cc_report] to dump cc report!");
     //API::dump_cc_report(&report.cc_report);
