1b03fc7f9740b3fcba9a064f59538e5341a17a7e
hairongchen <hairong.chen@intel.com>
2024-01-29 16:32:54 +0800

    c22

--- vmsdk/rust/cctrusted_vm/src/sdk.rs
+++ vmsdk/rust/cctrusted_vm/src/sdk.rs
@@ -519,7 +519,7 @@ mod sdk_api_tests {
                 break;
             }
         }
-        println!("==== number_of_eventlogs = {}", number_of_eventlogs);
+        //println!("==== number_of_eventlogs = {}", number_of_eventlogs);
 
         assert_eq!(eventlogs.len(), number_of_eventlogs);
     }
