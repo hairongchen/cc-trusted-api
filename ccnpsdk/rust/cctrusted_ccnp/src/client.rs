d0f808f00b6d9dc9e0a4e9631a6eb8b1bdd76eb7
hairongchen <hairong.chen@intel.com>
2024-02-05 10:05:19 +0800

    c10

--- ccnpsdk/rust/cctrusted_ccnp/src/client.rs
+++ ccnpsdk/rust/cctrusted_ccnp/src/client.rs
@@ -90,7 +90,7 @@ impl CcnpServiceClient {
 
         //let mut ccnp_client = CcnpClient::new(channel);
 
-        let response = CLIENT.get_client().get_quote(request).await.unwrap().into_inner();
+        let response = get_client().get_quote(request).await.unwrap().into_inner();
         Ok(response)
     }
 
